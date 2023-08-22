730f40663cc3e487a311a3ffd1d35badc4e68d84
BeJunHee <nowaffle1753@gmail.com>
2023-08-22 20:54:02 +0900

    feat : header 구조 진행
    header 의 html 구조 및 css적용 완료
    header의 js작업 진행 예정

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 // dom (html 태그 로딩 완료 후 실행)
-$(document).ready(function () { });
+$(document).ready(function () {
+  // 클래스 nav 의 li 를 찾아라
+  // 저장한다. 재활용하기 위해서
+  // var boxs = $(".nav ul li"); //<- 사용하지 말라
+  const navLis = $(".nav ul li");
+  // let boxs = $(".nav ul li"); // <- 사용하지 말라(const가 에러가 나면 사용)
+});
 // 멀티미디어 리소스 로딩 완료 후 실행
-window.onload = function(){}
+window.onload = function () {
+  // 클래스 nav 의 li 를 찾아라
+  // document.querySelector(".nav ul li");// <- 잘못된놈
+  document.querySelectorAll(".nav ul li");
+  // var boxs = document.querySelectorAll(".nav ul li"); // <- 사용하지 말라
+  const navLis = document.querySelectorAll(".nav ul li");
+  // let boxs = document.querySelectorAll(".nav ul li"); // <- 사용하지 말라(const가 에러가 나면 사용)
+};
